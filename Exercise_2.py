62f6f6766e4950d4a7637fb0d3f9a261ec7094e6
DoanTony <doantony90@gmail.com>
2016-11-26 19:58:56 -0500

    commiting exercise

---
+++ Exercise_2.py
@@ -0,0 +1,9 @@
+
+number = int(input("Enter a number :"))
+
+if number%4 == 0:
+    print("This is a divider of 4")
+elif number%2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
